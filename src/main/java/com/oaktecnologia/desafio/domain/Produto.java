b6dc7d4cfa0fafb723f07957d222fa546c9d418c
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-06-21 18:41:30 -0300

    Criação da entidade de domínio:Produto

---
+++ src/main/java/com/oaktecnologia/desafio/domain/Produto.java
@@ -0,0 +1,89 @@
+package com.oaktecnologia.desafio.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Produto implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private String descricao;
+	private Double valor;
+	private boolean disponivel;
+	
+	public Produto() {
+	}
+
+	public Produto(String nome, String descricao, Double valor, boolean disponivel,Long id) {
+		super();
+		this.nome = nome;
+		this.descricao = descricao;
+		this.valor = valor;
+		this.disponivel = disponivel;
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public Double getValor() {
+		return valor;
+	}
+
+	public boolean isDisponivel() {
+		return disponivel;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+
+	public void setDisponivel(boolean disponivel) {
+		this.disponivel = disponivel;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+	
+	
+
+}
